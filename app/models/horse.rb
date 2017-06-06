fd94861a62405cbce07d907fff587a7a7405cc1b
Tsuyoshi Tanaka <tanakatsu1080@gmail.com>
2017-06-07 06:30:59 +0900

    Add validations to model

--- app/models/horse.rb
+++ app/models/horse.rb
@@ -1,2 +1,7 @@
 class Horse < ApplicationRecord
+  validates :name, presence: true, uniqueness: { scope: :date }
+  validates :url, presence: true
+  validates :date, presence: true
+  validates :image_url, presence: true
+  validates :race, presence: true
 end
