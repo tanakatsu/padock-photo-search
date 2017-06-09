b8fca65ea1ed3ea15d086c7b50673fd37dcdf77b
Tsuyoshi Tanaka <tanakatsu1080@gmail.com>
2017-06-10 08:41:04 +0900

    Refactor routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :search, only: [:index] do
+  resource :search, only: [], controller: :search do
     collection do
+      get :index
       get :show_results
     end
   end
